5784cb6d95767b4174495017abf87ba28adc7de4
han <ynsluhan@foxmail.com>
2021-07-25 00:32:00 +0800

    init

---
+++ Thread.go
@@ -0,0 +1,66 @@
+package Thread
+
+import (
+	"github.com/manucorporat/stats"
+	"log"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var (
+	ips        = stats.New()
+	messages   = stats.New()
+	users      = stats.New()
+	mutexStats sync.RWMutex
+	savedStats map[string]uint64
+)
+
+// 设置操作系统线程数。
+func ConfigRuntime() {
+	nuCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(nuCPU)
+	log.Printf("Running with %d CPUs", nuCPU)
+}
+
+func StatsWorker() {
+	c := time.Tick(1 * time.Second)
+	var lastMallocs uint64
+	var lastFrees uint64
+	for range c {
+		var stats runtime.MemStats
+		runtime.ReadMemStats(&stats)
+
+		mutexStats.Lock()
+		savedStats = map[string]uint64{
+			"timestamp":  uint64(time.Now().Unix()),
+			"HeapInuse":  stats.HeapInuse,
+			"StackInuse": stats.StackInuse,
+			"Mallocs":    stats.Mallocs - lastMallocs,
+			"Frees":      stats.Frees - lastFrees,
+			"Inbound":    uint64(messages.Get("inbound")),
+			"Outbound":   uint64(messages.Get("outbound")),
+			"Connected":  connectedUsers(),
+		}
+		lastMallocs = stats.Mallocs
+		lastFrees = stats.Frees
+		messages.Reset()
+		mutexStats.Unlock()
+	}
+}
+
+func connectedUsers() uint64 {
+	connected := users.Get("connected") - users.Get("disconnected")
+	if connected < 0 {
+		return 0
+	}
+	return uint64(connected)
+}
+
+// Stats returns savedStats data.
+func Stats() map[string]uint64 {
+	mutexStats.RLock()
+	defer mutexStats.RUnlock()
+
+	return savedStats
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ynsluhan/go-new-thread
+
+go 1.16
+
+require github.com/manucorporat/stats v0.0.0-20180402194714-3ba42d56d227
